d8c7b2f5315dd2c7e93641388daa95732168e0db
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2025-01-04 22:34:40 +0530

    resume google drive link

--- components/ui/DownloadResume.tsx
+++ components/ui/DownloadResume.tsx
@@ -6,7 +6,7 @@ const DownloadResume: React.FC = () => {
 
   return (
     <Link
-      href="https://drive.google.com/file/d/1uY4DzOZAb_byd1nL4ylxYaSCGa5kmvTR/view?usp=sharing"
+      href="https://drive.google.com/file/d/1flo7f2Ml7n44ex3g8rvAlmQ5kXR4dcVV/view?usp=sharing"
       className="flex items-center justify-center px-3 py-2 bg-white/20 backdrop-blur-md text-white rounded-md border border-white/10 shadow-lg transition-transform duration-300 hover:bg-white/30 hover:scale-105 hover:cursor-pointer font-medium"
       target="_blank"
     >
