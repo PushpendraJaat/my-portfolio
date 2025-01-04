aff1ba761070f8ff6d0f1fc5ddec67d14f570945
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2025-01-04 22:19:44 +0530

    fix: update href in DownloadResume component to point to the correct resume link

--- components/ui/DownloadResume.tsx
+++ components/ui/DownloadResume.tsx
@@ -6,7 +6,7 @@ const DownloadResume: React.FC = () => {
 
   return (
     <Link
-      href=""
+      href="https://drive.google.com/file/d/1uY4DzOZAb_byd1nL4ylxYaSCGa5kmvTR/view?usp=sharing"
       className="flex items-center justify-center px-3 py-2 bg-white/20 backdrop-blur-md text-white rounded-md border border-white/10 shadow-lg transition-transform duration-300 hover:bg-white/30 hover:scale-105 hover:cursor-pointer font-medium"
       target="_blank"
     >
